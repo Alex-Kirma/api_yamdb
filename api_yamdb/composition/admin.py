5a5e4b96bf13ddd2cfdf591047306d7311626b8a
SergSukh <s9626265386@gmail.com>
2022-03-30 14:13:47 +0400

    fix to save, hasn`t migrations

--- api_yamdb/composition/admin.py
+++ api_yamdb/composition/admin.py
@@ -1,3 +1,16 @@
+from platform import release
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Genres, Titles, Categories
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title','author', 'release', 'title_urls')
+    search_fields = ('title', 'author')
+    list_filter = ('release',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Titles, TitlesAdmin)
+admin.site.register(Genres)
+admin.site.register(Categories)
--- api_yamdb/composition/models.py
+++ api_yamdb/composition/models.py
@@ -1,9 +1,8 @@
-# from django.contrib.auth import get_user_model
-from unicodedata import category
 from django.db import models
 
 
 class Author(models.Model):
+    """Модель Автор для будущих расширений"""
     first_name = models.TextField
     last_name = models.TextField
     slug = models.SlugField
@@ -13,6 +12,7 @@ class Author(models.Model):
 
 
 class Genres(models.Model):
+    """Модель жанры, мнгое кмногому"""
     genre = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField
@@ -22,6 +22,7 @@ class Genres(models.Model):
 
 
 class Categories(models.Model):
+    """Модель категории одно к многим """
     category = models.CharField(max_length=80)
     slug = models.SlugField(unique=True)
     description = models.TextField
@@ -31,7 +32,13 @@ class Categories(models.Model):
 
 
 class Titles(models.Model):
+    """Модель Произведение, базовая модель"""
     title = models.TextField
+    title_urls = models.URLField(
+        unique=True,
+        blank=True,
+        null=True
+    )
     author = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
@@ -40,5 +47,20 @@ class Titles(models.Model):
         null=True
     )
     relese = models.DateField('Дата публикации')
-    genre = models.ForeignKey(Genres, on_delete=models.CASCADE, null=False, blank=False)
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='title'
+    )
+    categories = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='title'
+    )
     
+    class Meta:
+        ordering = -['release']
