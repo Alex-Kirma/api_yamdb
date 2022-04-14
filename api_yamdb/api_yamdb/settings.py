b82c307261bc7832ebcacdae9be94d4dfee120fa
kirma133113 <Kirma@161.ru>
2022-04-14 16:45:27 +0300

    final_fix_2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,6 +5,7 @@ from api_yamdb.secret_settings import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
